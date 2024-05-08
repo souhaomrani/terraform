2ccc2e5353f0da819e03da1721355eadb25f5e8e
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-05-08 22:26:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,26 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
-       version = "3.0.1-rc1"
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc1"
     }
-  }
-
-  backend "http" {
-    # Configure HTTP backend as needed
   }
 }
 
 provider "proxmox" {
-  pm_api_url          = var.proxmox_url
-  pm_api_token_secret = var.api_token
-  pm_api_token_id     = var.api_id
+  pm_api_url         = "https://192.168.127.134:8006/api2/json"
+  pm_user            = "root@pam!souhasouha"
+  pm_password        = "faf0b3f2-8df5-48ec-b726-6fadc60aa5d1"
+  pm_tls_insecure    = true
+}
+
+resource "proxmox_vm_qemu" "terraform-test" {
+  name        = "terraform-test"                               # Nom de la machine virtuelle
+  target_node = "pve"                                           # Noeud Proxmox où déployer la machine virtuelle
+  iso         = "local:iso/ubuntu-22.04.3-desktop-amd64.iso"    # Nom du fichier ISO pour l'installation
+
+  # Configuration optionnelle pour l'agent QEMU Guest
+  agent {
+    enabled = true
+  }
 }
