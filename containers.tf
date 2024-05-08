0a026c5cdc421813d82316c9ad8c22662f8d1f25
souhaomrani <159710524+souhaomrani@users.noreply.github.com>
2024-05-08 19:52:37 +0200

    Update containers.tf

--- containers.tf
+++ containers.tf
@@ -1,5 +1,5 @@
 resource "proxmox_lxc" "terraform-test" {
-  target_node  = "pve01"                    # Hôte Proxmox sur lequel créer le conteneur
+  target_node  = "pve"                    # Hôte Proxmox sur lequel créer le conteneur
   hostname     = "terraform-test"           # Nom du conteneur
   ostemplate   = "local:vztmpl/almalinux-9-default_20221108_amd64.tar.xz"  # Chemin vers le modèle de conteneur
   password     = "terraform"                # Mot de passe du conteneur
